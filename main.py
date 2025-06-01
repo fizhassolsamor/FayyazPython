2ab9351a5b5db8dbe88013787aa4a0f5913207c8
fizhassolsamor <fizhassolsamor@gmail.com>
2025-06-01 15:01:01 +0500

    Added Comments

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+'''
+1.	Open CMD.exe.
+2.	Go to desired directory (Project folder will be created here if you are pulling from GitHub).
+3.	Git Clone https://github.com/fizhassolsamor/FayyazPython.git (Link can copied from GitHub).
+4.	Check the location on Browser there will be “git” Folder (Hidden sometimes) and “README.md” file.
+5.	Copy paste the source files ( main.py and Requirements.txt) into the cloned folder ).
+6.	Type git status – it will show you main.py and Requirements.txt as untracked files in red.
+7.	Type git add main.py – and press Enter.
+8.	Type git add Requirements.txt – and press Enter.
+9.	Type git status – it will show you main.py and Requirements.txt in green as new file.
+10.	Type git commit -m “Adding PythonGit Poject” – It will show you both the files as create mode with code.
+11.	Type git push – and press Enter.
+
+'''
 print ('hellooo')
